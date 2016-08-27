c4352c14ff9068998ddef9be2e60ec48c14d80be
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-08-28 00:38:12 +0300

    generate schema

--- data/database.js
+++ data/database.js
@@ -1,9 +1,10 @@
 /* eslint-disable arrow-body-style */
-import {
-  // User,
-  Post,
-} from '../models';
+// import {
+//   // User,
+//   Post,
+// } from '../models';
 
+export class Post {}
 export class User {}
 
 // Mock authenticated ID
@@ -16,23 +17,24 @@ const usersById = {
   [VIEWER_ID]: viewer,
 };
 
-export function getPost(id) {
-  Post.findById(id)
-    .then(post => {
-      return post;
-    }).catch(err => {
-      if (err) throw err;
-    });
-}
-
-export function getPosts() {
-  Post.findAll()
-    .then(posts => {
-      return posts;
-    }).catch(err => {
-      if (err) throw err;
-    });
-}
+//
+// export function getPost(id) {
+//   Post.findById(id)
+//     .then(post => {
+//       return post;
+//     }).catch(err => {
+//       if (err) throw err;
+//     });
+// }
+//
+// export function getPosts() {
+//   Post.findAll()
+//     .then(posts => {
+//       return posts;
+//     }).catch(err => {
+//       if (err) throw err;
+//     });
+// }
 
 // export function getUser(id) {
 //
@@ -43,10 +45,76 @@ export function getPosts() {
 // }
 
 
+// Mock Post data
+const postsById = {};
+const postIdsByUser = {
+  [VIEWER_ID]: [],
+};
+let nextPostId = 0;
+addPost('Taste JavaScript', true);
+addPost('Buy a unicorn', false);
+
+export function addPost(text, complete) {
+  const post = new Post();
+  post.complete = !!complete;
+  post.id = `${nextPostId++}`;
+  post.text = text;
+  postsById[post.id] = post;
+  postIdsByUser[VIEWER_ID].push(post.id);
+  return post.id;
+}
+
+export function changePostStatus(id, complete) {
+  const post = getPost(id);
+  post.complete = complete;
+}
+
+export function getPost(id) {
+  return postsById[id];
+}
+
+export function getPosts(status = 'any') {
+  const posts = postIdsByUser[VIEWER_ID].map(id => postsById[id]);
+  if (status === 'any') {
+    return posts;
+  }
+  return posts.filter(post => post.complete === (status === 'completed'));
+}
+
 export function getUser(id) {
   return usersById[id];
 }
 
 export function getViewer() {
   return getUser(VIEWER_ID);
+}
+
+export function markAllPosts(complete) {
+  const changedPosts = [];
+  getPosts().forEach(post => {
+    if (post.complete !== complete) {
+      post.complete = complete;
+      changedPosts.push(post);
+    }
+  });
+  return changedPosts.map(post => post.id);
+}
+
+export function removePost(id) {
+  const postIndex = postIdsByUser[VIEWER_ID].indexOf(id);
+  if (postIndex !== -1) {
+    postIdsByUser[VIEWER_ID].splice(postIndex, 1);
+  }
+  delete postsById[id];
+}
+
+export function removeCompletedPosts() {
+  const postsToRemove = getPosts().filter(post => post.complete);
+  postsToRemove.forEach(post => removePost(post.id));
+  return postsToRemove.map(post => post.id);
+}
+
+export function renamePost(id, text) {
+  const post = getPost(id);
+  post.text = text;
 }
