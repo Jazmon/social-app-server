936a79468f786d8cc982b332c6de78dcb7b4ba89
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-08-28 16:47:22 +0300

    fix schema

--- data/schema.js
+++ data/schema.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-use-before-define, no-param-reassign */
+/* eslint-disable no-use-before-define, no-param-reassign, consistent-return, arrow-body-style */
 import {
   GraphQLBoolean,
   GraphQLID,
@@ -31,14 +31,61 @@ import { resolver } from 'graphql-sequelize';
   // getUser,
   // getViewer,
 // } from './database';
-
 import { Post, User } from '../models';
+
+const getPosts = async() => {
+  try {
+    const posts = await Post.findAll();
+    const postObjs = posts.map(post => post.get());
+
+    return postObjs;
+  } catch (e) {
+    if (e) throw e;
+  }
+};
+
+const getPost = async(id) => {
+  try {
+    const post = await Post.findById(id);
+    return post.get();
+  } catch (e) {
+    if (e) throw e;
+  }
+};
+
+const getUser = async(id) => {
+  try {
+    const user = await User.findById(id, {
+      attributes: ['id', 'email', 'username', 'name'],
+      include: [{
+        model: Post,
+        as: 'posts',
+        attributes: ['id', 'text'],
+      }],
+    });
+    return user.get();
+  } catch (e) {
+    if (e) throw e;
+  }
+};
+
+// function getPosts() {
+//   Post.findAll()
+//     .then(posts => { // eslint-disable-line arrow-body-style
+//       const postObjs = posts.map(post => post.get());
+//       console.log('postObjs', postObjs);
+//       return postObjs;
+//     })
+//     .catch(err => {
+//       throw err;
+//     });
+// }
 
 const { nodeInterface, nodeField } = nodeDefinitions(
   (globalId) => {
     const { type, id } = fromGlobalId(globalId);
     if (type === 'Post') {
-      resolver(Post, {
+      return resolver(Post, {
         before: (options, args) => {
           options.where = options.where || {};
           options.where.id = { $in: args.ids };
@@ -48,7 +95,7 @@ const { nodeInterface, nodeField } = nodeDefinitions(
       // return getPost(id);
     } else if (type === 'User') {
       console.log('user resolver');
-      resolver(User, {
+      return resolver(User, {
         before: (options, args) => {
           console.log('user resolver before');
           options.where = options.where || {};
@@ -77,15 +124,16 @@ const GraphQLPost = new GraphQLObjectType({
     id: globalIdField('Post'),
     text: {
       type: GraphQLString,
-      resolve: (obj) => obj.text,
+      description: 'the content of the post',
+      resolve: (obj) => console.log('obj', obj) || obj.text,
     },
   },
   interfaces: [nodeInterface],
 });
 
 const {
-  connectionType: PostsConnection,
-  edgeType: GraphQLPostEdge,
+  connectionType: PostConnection,
+  edgeType: PostEdge,
 } = connectionDefinitions({
   name: 'Post',
   nodeType: GraphQLPost,
@@ -97,31 +145,33 @@ const GraphQLUser = new GraphQLObjectType({
     id: globalIdField('User'),
     email: {
       type: GraphQLString,
-      resolve: (obj) => console.log(obj) || obj.email,
+      description: 'the email of the user',
+      resolve: (user) => user.email,
     },
     posts: {
-      type: PostsConnection,
-      args: {
-        ...connectionArgs,
-      },
-      resolve: (obj, args) =>
-        connectionFromArray(resolver(Post, {
-          before: (options, args2) => {
-            if (args2.first) {
-              options.order = options.order || [];
-              options.order.push(['created_at', 'ASC']);
-
-              if (args2.first !== 0) {
-                options.limit = args2.first;
-              }
-            }
-            return options;
-          }
-        }), args),
+      type: PostConnection,
+      args: connectionArgs,
+      resolve: async(user, args) => {
+        let posts = await user.getPosts();
+        posts = posts.map(post => post.get());
+        console.log('posts', posts);
+        return connectionFromArray(posts, args);
+      }
     },
+    // posts: {
+    //   type: new GraphQLList(GraphQLPost),
+    //   args: {},
+    //   resolve: async(root, args) => {
+    //     return await getPosts();
+    //   }
+    // },
     postCount: {
       type: GraphQLInt,
-      resolve: resolver(Post).length,
+      resolve: async(user, args) => {
+        const posts = await user.getPosts();
+        console.log('posts', posts);
+        return posts.length;
+      },
     },
   },
   interfaces: [nodeInterface],
